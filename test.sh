82ce84888899dbb477d3ac0247e797ccf0188571
Marie <mari.nazaryan7173@gmail.com>
2025-03-20 15:00:07 +0400

    test

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-
 for i in {1..20}
 do
-	args=$(ruby -e "puts (1..$1).to_a.shuffle.join(' ')")
+	args=$(python3 -c "import random; print(' '.join(map(str, random.sample(range(-$1, $1), $1))))")
 
 	instruction_count=$(./push_swap $args | wc -l)
-	test_ok=$(./push_swap $args | ./checker_Mac $args)
+
+	test_ok=$(./push_swap $args | ./checker $args)
 
 	echo "Number of instructions: $instruction_count
 	Is sorted?: $test_ok"
